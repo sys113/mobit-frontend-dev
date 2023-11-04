d929f02cb8103306b8fb04c69a1c97a57f79267a
sys113 <051.sys113@gmail.com>
2023-11-04 14:38:21 +0330

    lib: add required libs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: { not: /\.(css|scss|sass)$/ },
+        resourceQuery: { not: /url/ },
+        loader: "@svgr/webpack",
+      },
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
